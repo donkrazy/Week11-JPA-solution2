89682a773b3bd40da18ca6660eb6257918e19026
kickscar <kickscar@gmail.com>
2016-05-17 13:48:31 +0900

    GuestbookRepository [QueryDSL] 구현

--- src/main/java/com/estsoft/mysite/repository/GuestbookRepository.java
+++ src/main/java/com/estsoft/mysite/repository/GuestbookRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+
 import com.estsoft.mysite.domain.Guestbook;
+import static com.estsoft.mysite.domain.QGuestbook.guestbook;
+import com.mysema.query.jpa.impl.JPAQuery;
 
 @Repository
 public class GuestbookRepository {
@@ -21,26 +23,42 @@ public class GuestbookRepository {
 		em.persist( guestbook );
 	}
 	
-	public Boolean remove( Guestbook guestbook ) {
+	public Boolean remove( Guestbook gb ) {
+		/* JPQL		
 		TypedQuery<Guestbook> query = 
 				em.createQuery( 
 		"select gb from Guestbook gb where gb.no = :no and gb.password = :password", Guestbook.class );
 		query.setParameter( "no", guestbook.getNo() );
 		query.setParameter( "password", guestbook.getPassword() );
-		
 		List<Guestbook> list = query.getResultList();
-		if( list.size() != 1 ) {
+		*/
+		
+		// QueryDSL
+		Guestbook result = new JPAQuery( em ).
+			from( guestbook ).
+			where( guestbook.no.eq( gb.getNo() ), 
+				   guestbook.password.eq( gb.getPassword() ) ).
+			singleResult( guestbook );
+
+		if( result == null ) {
 			return false;
 		}
-
-		em.remove( list.get( 0 ) );
+		
+		em.remove( result );
 		return true;
 	}
 	
 	public List<Guestbook> findAll() {
+		/* JPQL
 		TypedQuery<Guestbook> query = 
 				em.createQuery( "select gb from Guestbook gb order by gb.regDate desc", Guestbook.class );
 		List<Guestbook> list = query.getResultList();
-		return list;
+		*/
+		
+		// QueryDSL
+		return new JPAQuery( em ).
+			from( guestbook ).
+			orderBy( guestbook.regDate.desc() ).
+			list( guestbook );
 	}
 }
